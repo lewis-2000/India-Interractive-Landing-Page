1cd7d0031e15fec389358c3f56fad6f9d03859c7
Lewis Ng'ang'a <flammylewis4@gmail.com>
2024-10-26 12:07:17 +0300

    Fixed Map marker icon not showing up

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import StateInfo from '../States/StateInfo';
+import L from 'leaflet';
 
 // Define the capitals' locations
 const capitals = [
@@ -35,6 +36,17 @@ const capitals = [
     { state: "Uttarakhand", capital: "Dehradun", coords: [30.3165, 78.0322] },
     { state: "West Bengal", capital: "Kolkata", coords: [22.5726, 88.3639] },
 ];
+
+// Create a custom marker icon
+const markerIcon = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
 
 const MapComponent: React.FC = () => {
     const [data, setData] = useState<any | null>(null);
@@ -118,7 +130,7 @@ const MapComponent: React.FC = () => {
 
     return (
         <div className="h-screen w-full z-10">
-            <MapContainer center={[20.5937, 78.9629]} zoom={5} className="h-full w-full" zoomControl={false} attributionControl={false} >
+            <MapContainer center={[20.5937, 78.9629]} zoom={5} className="h-full w-full" zoomControl={false} attributionControl={false}>
                 <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, &copy; <a href='https://carto.com/attributions'>CARTO</a>"
@@ -132,7 +144,7 @@ const MapComponent: React.FC = () => {
                 )}
                 {/* Add markers for capitals */}
                 {capitals.map((capital, index) => (
-                    <Marker key={index} position={capital.coords}>
+                    <Marker key={index} position={capital.coords} icon={markerIcon}>
                         <Popup>
                             <strong>{capital.capital}</strong><br />
                             {capital.state}
